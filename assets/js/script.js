d2f16b360590851ce47158d60000dca5562842da
Lucas Rafael <lucasrafaelinfo@gmail.com>
2024-10-24 22:41:33 -0300

    Information Lands - Versão 1

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+// Adiciona comportamento suave ao clicar nos links do menu
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        const target = document.querySelector(this.getAttribute('href'));
+        target.scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
